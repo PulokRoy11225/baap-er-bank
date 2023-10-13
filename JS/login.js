d373a9ffe2d38a30bcf875d949719d276b2604b5
PulokRoy11225 <pulokr221@gmail.com>
2023-10-14 00:12:50 +0800

    added event handler

---
+++ JS/login.js
@@ -0,0 +1,22 @@
+// step-1: add click event handler with the submit button
+document.getElementById('btn-submit').addEventListener('click', function(){
+  // console.log('submit button clicked');
+  // step-2: get the email address inside the email input field
+  // always remember to use .value to get text from an input field
+  const emailField = document.getElementById('user-email');
+  const email = emailField.value;
+  // console.log(email);
+  // step-3: get password
+  // 3.a: set id on the html element
+  // 3.b: get the element
+  // 3.c: get the value from the element
+  const passwordField = document.getElementById('user-password');
+  const password = passwordField.value;
+  // console.log(email,password);
+  // step-4: verify email and password
+  if(email==='pulokr221@gmail.com' && password==='pulok1'){
+    console.log('valid user')
+  }else{
+    console.log('invalid user')
+  }
+})
